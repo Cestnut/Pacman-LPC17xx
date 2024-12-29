ec94116b47ee9933d0b1c52ce5fbb12e8fab1762
Cestnut <hunterkudo@gmail.com>
2024-12-29 23:45:43 +0100

    removed unnecessary interrupts that were enabled

--- button_EXINT/lib_button.c
+++ button_EXINT/lib_button.c
@@ -17,11 +17,7 @@ void BUTTON_init(void) {
   LPC_GPIO2->FIODIR      &= ~(1 << 12);    /* PORT2.12 defined as input          */
 
   LPC_SC->EXTMODE = 0x7;
-
-  NVIC_EnableIRQ(EINT2_IRQn);              /* enable irq in nvic                 */
-	NVIC_SetPriority(EINT2_IRQn, 1);				 /* priority, the lower the better     */
-  NVIC_EnableIRQ(EINT1_IRQn);              /* enable irq in nvic                 */
-	NVIC_SetPriority(EINT1_IRQn, 2);				 
+				 
   NVIC_EnableIRQ(EINT0_IRQn);              /* enable irq in nvic                 */
 	NVIC_SetPriority(EINT0_IRQn, 3);				 /* decreasing priority	from EINT2->0	 */
 }
